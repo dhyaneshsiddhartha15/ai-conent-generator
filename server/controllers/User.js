47eb34c789b139e992b33e3a7d2297d95faeaff1
dhyaneshsiddhartha15 <siddharthadhyanesh7@gmail.com>
2024-07-22 08:52:30 +0530

    Authenticated

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -88,9 +88,10 @@ if(!user){
             expiresIn:"24h",
         });
         const options = {
-            expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
             httpOnly: true,
-            sameSite:"strict",
+        secure: true,
+        sameSite: "none",
+        maxAge: 24 * 60 * 60 * 1000,
         };
         return res.cookie("token",token,options).json({
             success:true,
